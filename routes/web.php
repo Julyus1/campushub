b05182f00dd53e7cd862716427a46289afb85e2a
julyus1 <jmorin0100@gmail.com>
2025-02-15 22:29:49 +0800

    added routes for department and course registration

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/', function () {
 });
 
 Route::get('admin', [AdminController::class, 'index']);
-Route::get('student-list', [AdminController::class, 'show']);
-Route::get('register-student', [AdminController::class, 'create']);
-Route::post('student', [AdminController::class, 'store']);
+Route::get('student-list', [AdminController::class, 'show_stud']);
+Route::get('register-student', [AdminController::class, 'create_stud']);
+Route::post('student', [AdminController::class, 'store_stud']);
+Route::get('register-course', [AdminController::class, 'show_course']);
+Route::get('register-department', [AdminController::class, 'show_department']);
