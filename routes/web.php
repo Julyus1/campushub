d2f1b02de7bdb67a253addd52a3da98c4e9c0791
julyus1 <jmorin0100@gmail.com>
2025-03-17 23:27:22 +0800

    Made routes for superadmin

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,29 @@ Route::middleware(['auth', 'userrole:1'])->group(function () {
     Route::get('superadmin/admin/list', [SuperadminController::class, 'admin_list']);
     Route::get('superadmin/faculty/list', [SuperadminController::class, 'faculty_list']);
     Route::get('superadmin/student/list', [SuperadminController::class, 'student_list']);
+
+    Route::get('superadmin/student/list', [SuperAdminController::class, 'show_stud']);
+    Route::get('superadmin/student/profile/{student}', [SuperAdminController::class, 'stud_profile']);
+    Route::get('superadmin/student/register', [SuperAdminController::class, 'create_stud']);
+    Route::post('superadmin/student/register', [SuperAdminController::class, 'store_stud']);
+    Route::get('superadmin/student/update/{student}', [SuperAdminController::class, 'stud_showupdate']);
+    Route::patch('superadmin/student/update/{student}', [SuperAdminController::class, 'stud_update']);
+    Route::delete('superadmin/student/delete/{student}', [SuperAdminController::class, 'stud_destroy']);
+
+    Route::get('superadmin/course/register', [SuperAdminController::class, 'show_course']);
+    Route::post('superadmin/course/add', [SuperAdminController::class, 'store_course']);
+    Route::patch('superadmin/course/update/{course}', [SuperAdminController::class, 'update_course']);
+    Route::delete('superadmin/course/delete/{course}', [SuperAdminController::class, 'destroy_course']);
+
+    Route::get('superadmin/department/register', [SuperAdminController::class, 'show_department']);
+    Route::post('superadmin/department/register', [SuperAdminController::class, 'store_department']);
+    Route::patch('superadmin/department/update/{department}', [SuperAdminController::class, 'update_department']);
+    Route::delete('superadmin/department/delete/{department}', [SuperAdminController::class, 'destroy_department']);
+
+    Route::get('superadmin/section/list', [AdminController::class, 'show_section']);
+    Route::post('superadmin/section/add', [AdminController::class, 'store_section']);
+    Route::patch('superadmin/section/update/{section}', [AdminController::class, 'update_section']);
+    Route::delete('superadmin/section/delete/{department}', [AdminController::class, 'destroy_section']);
 });
 
 Route::middleware(['auth', 'userrole:2'])->group(function () {
@@ -53,15 +76,12 @@ Route::middleware(['auth', 'userrole:2'])->group(function () {
     Route::patch('course/update/{course}', [AdminController::class, 'update_course']);
     Route::delete('course/delete/{course}', [AdminController::class, 'destroy_course']);
 
-    Route::get('department/register', [AdminController::class, 'show_department']);
-    Route::post('department/register', [AdminController::class, 'store_department']);
-    Route::patch('department/update/{department}', [AdminController::class, 'update_department']);
-    Route::delete('department/delete/{department}', [AdminController::class, 'destroy_department']);
 
-    Route::get('section/list', [SectionController::class, 'show']);
-    Route::post('section/add', [SectionController::class, 'store']);
-    Route::patch('section/update/{section}', [SectionController::class, 'update']);
-    Route::delete('section/delete/{department}', [SectionController::class, 'destroy']);
+
+    Route::get('section/list', [AdminController::class, 'show_section']);
+    Route::post('section/add', [AdminController::class, 'store_section']);
+    Route::patch('section/update/{section}', [AdminController::class, 'update_section']);
+    Route::delete('section/delete/{department}', [AdminController::class, 'destroy_section']);
 });
 Route::middleware(['auth', 'userrole:3'])->group(function () {
     Route::get('/faculty/dashboard', [FacultyController::class, 'index']);
